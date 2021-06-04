eccdf76961d99208659527021ef3368cf343529a
Brandon Gift <bagift10@gmail.com>
2021-06-04 13:38:16 -0400

    Initial Contacts project commit created with create-react-app
    Implemented a ContactList component that receives a "contacts" prop to return an ordered list of all contacts in the array.

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class ContactList extends Component {
+  render () {
+    const people = this.props.contacts;
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <ContactList
+        contacts={[
+          { name: 'Tyler'},
+          { name: 'Karen'},
+          { name: 'Richard'}
+        ]}
+      />
+      <ContactList
+        contacts={[
+          { name: 'Amanda'},
+          { name: 'Mikenzi'},
+          { name: 'Ryan'}
+        ]}
+      />
+    </div>
+  );
+}
+
+export default App;
